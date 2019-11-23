f67427d67cd65b1460d45ff31c2c0c47e27fa2c5
crechet <alexander@macbook.local>
2019-11-23 13:12:32 +0300

    Update App.js, add Root.js

---
+++ src/Root.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ * @flow
+ */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Root = () => {
+    return (
+        <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+        >
+            <Text>Root</Text>
+        </View>
+    );
+};
+
+export default Root;
