1f2bc7753177ee57ca3a18034295d648ffc3dd55
crechet <alexander@macbook.local>
2019-11-23 13:22:37 +0300

    Use SafeAreaProvider at Root component

--- src/Root.js
+++ src/Root.js
@@ -4,14 +4,17 @@
  */
 import React from 'react';
 import { View, Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Root = () => {
     return (
-        <View
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
-        >
-            <Text>Root</Text>
-        </View>
+        <SafeAreaProvider>
+            <View
+                style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+            >
+                <Text>Root</Text>
+            </View>
+        </SafeAreaProvider>
     );
 };
 
