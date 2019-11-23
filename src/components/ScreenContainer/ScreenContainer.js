a2ea17f4cf37af428c98e673ee550859b766d216
crechet <alexander@macbook.local>
2019-11-23 13:49:20 +0300

    Add ScreenContainer component

---
+++ src/components/ScreenContainer/ScreenContainer.js
@@ -0,0 +1,54 @@
+/**
+ * @flow
+ */
+import * as React from 'react';
+import { StatusBar } from 'react-native';
+import type { ViewStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
+import SafeAreaView from 'react-native-safe-area-view';
+
+import styles from './ScreenContainerStyles';
+
+type Props = {
+    safeAreaViewStyles?: ViewStyleProp,
+    safeAreaForceInset?: {
+        top?: 'always' | 'never',
+        bottom?: 'always' | 'never',
+        left?: 'always' | 'never',
+        right?: 'always' | 'never',
+        vertical?: 'always' | 'never',
+        horizontal?: 'always' | 'never',
+    },
+    children?: React.Node
+};
+
+const ScreenContainer = (props: Props) => {
+    const { safeAreaForceInset } = props;
+    const safeAreaStyles = [styles.safeArea];
+
+    if (props.safeAreaViewStyles) safeAreaStyles.push(props.safeAreaViewStyles);
+
+    return (
+        <SafeAreaView
+            style={safeAreaStyles}
+            forceInset={safeAreaForceInset}
+        >
+            <StatusBar
+                barStyle='dark-content'
+                translucent
+                backgroundColor={'rgba(0, 0, 0, 0)'}
+            />
+
+            {props.children}
+        </SafeAreaView>
+    );
+};
+
+ScreenContainer.defaultProps = {
+    safeAreaBackgroundColor: styles.safeArea.backgroundColor,
+    safeAreaForceInset: {
+        top: 'always',
+        horizontal: 'never'
+    }
+};
+
+export default ScreenContainer;
---
+++ src/components/ScreenContainer/ScreenContainerStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../styles/commonStyles';
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+    },
+});
+
+export default styles;
